eaf80d06d966baa5eb10ec4ef53e4b507b5bb14f
nataliashashina <sergeevnaa01@mail.ru>
2023-03-11 12:44:05 +0300

    OOP part2 v1

---
+++ src/Truck.java
@@ -0,0 +1,9 @@
+public class Truck extends Car{
+    public Truck() {
+        super();
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
---
+++ src/WeServe.java
@@ -0,0 +1,4 @@
+public interface WeServe {
+    public void weServe(Bicycle bicycle);
+
+}
